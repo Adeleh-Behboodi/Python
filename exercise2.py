95276cd8db2dc149eb02951ce223f0fa52637d9b
Adeleh Behboodi <shelmanadelehbehboudi@gmail.com>
2022-12-11 19:21:23 +0330

    Initial commit

---
+++ exercise2.py
@@ -0,0 +1,17 @@
+print("Day 1 - string Manipulation")
+print("String concatenation is done with the" + "sign .")
+print('e.g.print ("Hello"+ " world")')
+print(("New lines can be created with a backslash and n."))
+a=3
+b=5
+a = input("a: ")
+b = input("b: ")
+c=a
+a=b
+b=c
+print("a: " +a)
+print("b: " +b)
+print("Congratulation")
+city = input("What are you from? ")
+animal=input ("What pet do you have? ")
+print ("city: " + city + "       Pet: " + animal)
